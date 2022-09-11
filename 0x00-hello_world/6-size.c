8f038e7ebe5ba252557db0ef82708cf24cf1f9bd
Smatarah <suzanmatarah305@gmail.com>
2022-09-11 01:06:26 -0700

    Size is not grandeur

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,6 +3,7 @@
 /**
  * main-Print size of various types
  *
+ * Description: using the main function
  * Return:0
  */
 int main(void)
@@ -13,11 +14,11 @@ int main(void)
 	long long int d;
 	float e;
 
-	printf("size of a char:%dbyte(s)\n",sizeof(a));
-	printf("size of an int:%dbyte(s)\n",sizeof(b));
-	printf("size of a long int:%dbyte(s)\n",sizeof(c));
-	printf("size of a long long int%dbyte(s)\n",sizeof(d));
-	printf("size of a float:%dbyte(s)\n",sizeof(e));
-	return(0)
+	printf("size of a char: %ldbyte(s)\n",sizeof(a));
+	printf("size of an int: %ldbyte(s)\n",sizeof(b));
+	printf("size of a long int: %ldbyte(s)\n",sizeof(c));
+	printf("size of a long long int %ldbyte(s)\n",sizeof(d));
+	printf("size of a float: %ldbyte(s)\n",sizeof(e));
+	return(0);
 }
 
