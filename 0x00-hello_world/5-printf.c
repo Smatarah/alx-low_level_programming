6763d3f205eb762c0b1e7f483ba602c6bedeb13b
Smatarah <suzanmatarah305@gmail.com>
2022-09-11 00:21:46 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,11 +3,11 @@
 /**
  * main-main block
  *
- * return:0
+ * return:0 (Successfuly)
  */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return(0);
+	return (0);
 }
 
