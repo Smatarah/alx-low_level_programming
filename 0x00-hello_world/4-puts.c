01b440c7e84b30c3e95c12bc8c51be6804410d07
Smatarah <suzanmatarah305@gmail.com>
2022-09-11 00:02:57 -0700

    Helo, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,9 +5,9 @@
  *
  * Return: 0 (Successful)
  */
-int main (void)
+int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
+	puts ("\"Programming is like building a multilingual puzzle");
 	return(0);
 }
 
