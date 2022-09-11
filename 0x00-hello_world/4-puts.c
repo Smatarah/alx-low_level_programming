1436a330d48943567c6a7dcd713a70eb62a6107c
Smatarah <suzanmatarah305@gmail.com>
2022-09-10 23:42:54 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - printing
+ *
+ * Return: 0 (Successful)
+ */
+int main (void)
+{
+	Puts("\"Programming is like building a multilingual puzzle");
+	return (0)
+}
+
